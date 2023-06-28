086a4ea446b681beae93d362ba4f64b8fa94de1a
chennaiah birru <root@ubuntu1804.localdomain>
2023-06-28 19:48:22 +0000

    updated file

--- create_container.py
+++ create_container.py
@@ -3,10 +3,10 @@ import json
 
 def spin_container(host):
     # Spin up a container for the given host
-    container_id = subprocess.check_output(['docker', 'run', '-d', '-it', 'container_image_name']).strip().decode()
+    container_id = subprocess.check_output(['docker', 'run', '-d', '-it', 'demo-runner:1.0']).strip().decode()
 
     # Run playbook inside the container using Ansible runner
-    ansible_command = f'docker exec {container_id} ansible-runner run /opt/logs.yaml'
+    ansible_command = f'docker exec {container_id} ansible-runner run /opt/demo-runner/logs.yaml'
     subprocess.run(ansible_command, shell=True)
 
     # Stop and remove the container
@@ -14,7 +14,7 @@ def spin_container(host):
     subprocess.run(['docker', 'rm', container_id])
 
 def main():
-    inventory_file = '/opt/inv-env'
+    inventory_file = '/opt/demo-runner/inv-env'
 
     # Read the inventory file
     with open(inventory_file, 'r') as f:
