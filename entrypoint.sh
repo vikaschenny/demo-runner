eef316badbe0f1245ad1735807aa2e1e0863f5c0
root <root@ubuntu2204.localdomain>
2023-06-30 14:47:12 +0000

    update docker file and entry sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+#!/bin/bash
+
+inventory_file="inventory.ini"
+
+echo "[legacy]" > "/opt/$inventory_file"
+echo "$1" >> "/opt/$inventory_file"
+echo "" >> "/opt/$inventory_file"
+
+echo "[legacy:vars]" >> "/opt/$inventory_file"
+echo "ansible_user=$2" >> "/opt/$inventory_file"
+echo "ansible_password=$3" >> "/opt/$inventory_file"
+
+echo "Inventory file created successfully at /opt/$inventory_file."
 
 # Clone the GitHub repository
-git clone https://github.com/vikaschenny/demo-runner.git -b master /opt/
+git clone https://github.com/vikaschenny/demo-runner.git -b master 
 
 # Run additional commands or start your application here
-
+#ansible-runner run -p /opt/demo-runner/playbook.yml -i /opt/inventory.ini
+ansible-runner run /tmp/demo-runner -p /opt/demo-runner/playbook.yml -i inventory.ini
 # Execute the CMD or whatever command was passed to the container
-exec "$@"
+#exec "$@"
